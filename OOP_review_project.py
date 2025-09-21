8f59a122ea00e705451855d0440a04682eea9729
ThomasC <dradar77@gmail.com>
2025-09-21 11:34:42 -0400

    created the square class using the rectangle class as a foundation and simplifying it so l and w are equal

--- OOP_review_project.py
+++ OOP_review_project.py
@@ -84,5 +84,22 @@ class Rectangle(BasicShape):
         self.calc_area()
 
 
+class Square(Rectangle):
+    def __init__(self, s, n="square"):
+        self._side = s
+        super().__init__(s, s, n)
+        self.name = n
+
+    @property
+    def side(self):
+        return self._side
+
+    @side.setter
+    def side(self, value):
+        self._side = value
+        self.length = value
+        self.width = value
+        self.calc_area()
+
 
 
