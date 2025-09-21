33d9b5607f5f460300f770b8b6633abb3250995a
ThomasC <dradar77@gmail.com>
2025-09-21 09:59:11 -0400

    created parkedCar class to store all information about vehicles being inspected

--- OOP_review_project.py
+++ OOP_review_project.py
@@ -2,4 +2,28 @@
 ##OOP Review Project
 ##Parking_Ticket_Sim 
 ##9/21/25
+##This program simulates a parking ticket scenario using OOP principles.
 
+import math
+
+class ParkedCar:
+    def __init__(self, make, model, color, license_number, minutes_parked=60):
+        self.make = make
+        self.model = model
+        self.color = color
+        self.license_number = license_number
+        self.minutes_parked = 60
+        self.minutes_parked = minutes_parked
+
+        @property
+        def minutes_parked(self):
+            return self._minutes_parked
+
+        @minutes_parked.setter
+        def minutes_parked(self, value):
+            if value < 0:
+                raise ValueError("Minutes parked cannot be negative.")
+            self._minutes_parked = value
+
+        def __str__(self):
+            return f"{self.color} {self.make} {self.model}, License: {self.license_number}, Minutes Parked: {self.minutes_parked}"
