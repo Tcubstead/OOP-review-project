bf0b5476f26e4d03ac2c67eeac5a7a37a79eb275
ThomasC <dradar77@gmail.com>
2025-09-21 11:16:25 -0400

    created the basic shape class to define all shapes and give basic information storage

--- OOP_review_project.py
+++ OOP_review_project.py
@@ -1,4 +1,34 @@
-##Thomas Cubstead
-##OOP Review Project
-##Shapes_Hierarchy
-##9/21/25
+#Thomas Cubstead
+#OOP Review Project
+#Shapes_Hierarchy
+#9/21/25
+
+import math
+from abc import ABC, abstractmethod
+
+class BasicShape(ABC):
+    def __init__(self, name="shape"):
+        self.area = 0.0
+        self.name = name
+
+    @property
+    def area(self):
+        return self._area
+
+    @area.setter
+    def area(self, value):
+        self._area = value
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+        
+    @abstractmethod
+    def calc_area(self):
+        pass
+
+
