eb0f67850308d4f4782fbb84d5415191d659b361
ThomasC <dradar77@gmail.com>
2025-09-21 10:31:14 -0400

    created parking ticket class to format the illegally parked information and issue the fine with officer details

--- OOP_review_project.py
+++ OOP_review_project.py
@@ -43,3 +43,26 @@ class ParkingMeter:
             raise ValueError("Minutes purchased must be more than zero.")
         self._minutes_purchased = value
 
+class ParkingTicket:
+    def __init__(self, car, officer, illegal_minutes):
+        self.car = car
+        self.officer_name = officer.name
+        self.badge_number = officer.badge_number
+        self.illegal_minutes = illegal_minutes
+        self.fine = self.calculate_fine()
+
+    def calculate_fine(self):
+        hours = math.ceil(self.illegal_minutes /60)
+        if hours <= 1:
+            return 25.0
+        else:
+            return 25.0 + (hours - 1) * 10.0
+
+    def __str__(self):
+        return (f"=== PARKING TICKET ===\n"
+                f"Car: {self.car.make} {self.car.model}, color: {self.car.color}, license: {self.car.license_number}\n"
+                f"Illegally Parked for: {self.illegal_minutes} minutes\n"
+                f"Fine: ${self.fine:.2f}\n"
+                f"Issued by officer: {self.officer_name}, Badege #: {self.badge_number}\n")
+
+
